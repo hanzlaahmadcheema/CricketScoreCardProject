ba658f1021d76cb7453b8dca212d3cf5a92da16a
Bilawal Hussain <hbilawal590@gmail.com>
2024-12-28 13:44:34 +0500

    Update Title of Main

--- src/frontend/Main.java
+++ src/frontend/Main.java
@@ -2,7 +2,7 @@ package frontend;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Cricket Scorecard");
+        System.out.println("Welcome to Cricket Scorecard!");
         MainWindow mainWindow = new MainWindow();
         mainWindow.setVisible(true);
     }
